5ade935ae301e2a9f83495c495255202ff5ed766
Chua Hou <human+github@chuahou.dev>
2021-03-06 17:51:51 +0800

    Render: implement rendering of scenario (excluding hands)

--- src/Main.purs
+++ src/Main.purs
@@ -18,64 +18,25 @@ import Web.HTML.Window       (document)
 
 import Mahjong.Gen
 import Mahjong.Hand
+import Mahjong.Scenario
 import Render
-
--- | @RenderHand ts ns a@ is a hand to be rendered with drawn tiles @ts@, called
--- | tiles @ns@ and winning tile @a@.
-data RenderHand = RenderHand (Array Tile) (Array Tile) Tile
-
-toRenderHand :: Hand -> Maybe RenderHand
-toRenderHand h = case h of
-  Hand tt m1 m2 m3 a    -> goHand tt a $ splitKans <<< map snd $ [m1, m2, m3]
-  Tanki m1 m2 m3 m4 a   -> goTanki a   $ splitKans <<< map snd $ [m1, m2, m3, m4]
-  Chiitoi a b c d e f g -> Just $ RenderHand
-    ((concatMap (replicate 2) [a, b, c, d, e, f]) <> [g]) [] g
-  where
-    splitKans = foldr go (Tuple [] [])
-    go m@(Kantsu t) (Tuple ts ns) = Tuple ts (ns <> mentsuToArray m)
-    go m            (Tuple ts ns) = Tuple (ts <> mentsuToArray m) ns
-
-    goHand tt a (Tuple ts ns) = case join $ head <$> checkTatsu tt of
-      Just agari -> Just $ RenderHand (a:a:tatsuToArray tt <> ts) ns agari
-      _          -> Nothing
-    goTanki a (Tuple ts ns) = Just $ RenderHand (a:ts) ns a
 
 main :: Effect Unit
 main = do
-  -- Generate a hand
-  maybeRenderHand <- toRenderHand <$> genHand
-  RenderHand hand naki agari <- case maybeRenderHand of
-                                     Just rh -> pure rh
-                                     Nothing -> error "Invalid hand generated"
-                                             >>= \_ -> pure $ RenderHand [] [] East
-
   -- Get document node
   doc <- toParentNode <<< toDocument <$> (window >>= document)
   let query q = querySelector (QuerySelector q) doc
 
-  -- Display hand
-  query "#hand" >>= \x -> case x of
-    Just h -> foldl (addImg h) (pure unit) <<< sort $ hand
-    _      -> pure unit
-
-  -- Display called tiles
-  query "#naki" >>= \x -> case x of
-      Just n -> if null naki
-                  then setAttribute "style" "display:none" n
-                  else foldl (addImg n) (pure unit) <<< sort $ naki
-      _      -> pure unit
-
-  -- Display last tile
-  query "#agari" >>= \x -> case x of
-    Just a -> createImage agari >>= flip appendChild (toNode a)
-    _      -> pure unit
-
-  -- Display riichi stick if necessary
-  query "#riibou" >>= \x -> case x of
-    Just r  -> if true -- TODO
-                 then setAttribute "style" "visibility:visible" r
-                 else pure unit
-    Nothing -> pure unit
-
-  where
-    addImg d e x = e >>= \_ -> createImage x >>= flip appendChild (toNode d)
+  renderScenario query ({ hand:   Hand
+                                    (Ryanmen $ Manzu 2)
+                                    (closed $ Shuntsu $ Manzu 2)
+                                    (closed $ Shuntsu $ Manzu 3)
+                                    (open   $ Shuntsu $ Manzu 4)
+                                    (Manzu 9)
+                        , bakaze: South
+                        , jikaze: West
+                        , doras:  [ North, White ]
+                        , uras:   []
+                        , riichi: false
+                        , tsumo:  false
+                        })
--- src/Render.purs
+++ src/Render.purs
@@ -3,13 +3,17 @@
 
 module Render where
 
-import Effect       (Effect)
+import Data.Array      (null)
+import Data.Foldable   (foldl, traverse_)
+import Data.Maybe      (Maybe (..))
+import Effect          (Effect)
 import Prelude
-import Web.DOM.Node    (Node)
-import Web.DOM.Element (setClassName, toNode)
+import Web.DOM.Node    (Node, appendChild, setTextContent)
+import Web.DOM.Element (Element, setAttribute, setClassName, toNode)
 import Web.HTML.HTMLImageElement as Img
 
 import Mahjong.Hand (Tile (..))
+import Mahjong.Scenario
 
 -- | Generates filename for a tile.
 tileToFilename :: Tile -> String
@@ -35,3 +39,44 @@ createImage t = do
   let elem' = Img.toElement elem
   setClassName "tile" elem'
   pure <<< toNode $ elem'
+
+-- | Renders a scenario, given a function to query nodes by id with.
+renderScenario :: (String -> Effect (Maybe Element)) -> Scenario -> Effect Unit
+renderScenario q s = do
+  -- Render hands
+
+  -- Set winds
+  q' "#bakaze" $ traverse_ (setWind s.bakaze) <<< Img.fromElement
+  q' "#jikaze" $ traverse_ (setWind s.jikaze) <<< Img.fromElement
+
+  -- Add doras
+  q' "#dora" $ addTiles s.doras
+
+  -- Add uradoras, hide otherwise
+  q' "#ura" $ if null s.uras
+              then setAttribute "style" "visibility:hidden"
+              else addTiles s.uras
+
+  -- Add riichi stick
+  q' "#riibou" $ setAttribute "style" ("visibility:" <>
+                  if s.riichi
+                  then "visible"
+                  else "hidden")
+
+  -- Set tsumo/ron
+  q' "#tsumoron" $ setTextContent (if s.tsumo then "TSUMO" else "RON") <<< toNode
+  where
+    -- Run query and perform effect on any returned element, doing nothing
+    -- otherwise.
+    q' :: String -> (Element -> Effect Unit) -> Effect Unit
+    q' cs f = q cs >>= traverse_ f
+
+    -- Sets src of img to black variant of wind w.
+    setWind w img = Img.setSrc
+      ("./riichi-mahjong-tiles/Black/" <> tileToFilename w) img
+
+    -- Add tiles ts to div d.
+    addTiles :: Array Tile -> Element -> Effect Unit
+    addTiles ts d = foldl f (pure unit) ts
+      where
+        f n x = n >>= \_ -> createImage x >>= flip appendChild (toNode d)
